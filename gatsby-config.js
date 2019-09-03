e543d344c7656b2059d6356ab8020350da465231
Abreham <gezahegnabereham@gmail.com>
2019-09-03 10:01:54 -0400

    model section redesigned

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 const path = require(`path`)
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+console.log("process env:" ,process.env.CONTENTFUL_SPACE_ID)
 
 module.exports = {
   plugins: [
+  	 {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
